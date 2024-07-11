b11f8fdea749879ccd5433e4a2114105f1350055
BDS <113209967+BDS-001@users.noreply.github.com>
2024-07-11 12:42:40 -0400

    practice js in jsx

---
+++ src/JsWithJsx.jsx
@@ -0,0 +1,38 @@
+const person = {
+    name: 'Test Person',
+    theme: {
+      backgroundColor: 'black',
+      color: 'pink'
+    }
+  };
+
+export function Avatar() {
+    const avatar = '#';
+    const description = 'Description of the image';
+    return (
+      <img
+        className="test-image"
+        src={avatar}
+        alt={description}
+        style={{backgroundColor: 'black', color: 'pink'}}
+      />
+    );
+  }
+
+  export function TodoList() {
+    return (
+      <div style={person.theme}>
+        <h1>{person.name}&aposs Todos</h1>
+        <img
+          className="avatar"
+          src="#"
+          alt={person.name}
+        />
+        <ul>
+          <li>Improve the videophone</li>
+          <li>Prepare aeronautics lectures</li>
+          <li>Work on the alcohol-fuelled engine</li>
+        </ul>
+      </div>
+    );
+  }
