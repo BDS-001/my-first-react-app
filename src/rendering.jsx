975471dfb7078dab3ea32ae06bcc875d6d386313
BDS <113209967+BDS-001@users.noreply.github.com>
2024-07-12 12:52:29 -0400

    practice jsx rendering

--- src/rendering.jsx
+++ src/rendering.jsx
@@ -14,3 +14,28 @@ export default function Animals() {
   }
 
   //jsx can render arrays
+
+  function ListItem(props) {
+    return <li>{props.animal}</li>
+  }
+  
+  function List(props) {
+    return (
+      <ul>
+        {props.animals.map((animal) => {
+          return <ListItem key={animal} animal={animal} />;
+        })}
+      </ul>
+    );
+  }
+  
+  export function App() {
+    const animals = ["Lion", "Cow", "Snake", "Lizard"];
+  
+    return (
+      <div>
+        <h1>Animals: </h1>
+        <List animals={animals} />
+      </div>
+    );
+  }
