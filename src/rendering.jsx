beb6610c92f4028ae75b58f8107305fabfed002b
BDS <113209967+BDS-001@users.noreply.github.com>
2024-07-12 13:02:37 -0400

    practice conditional rendering

--- src/rendering.jsx
+++ src/rendering.jsx
@@ -20,6 +20,14 @@ export default function Animals() {
   }
   
   function List(props) {
+    if (!props.animals) {
+      return <div>Loading...</div>;
+    }
+  
+    if (props.animals.length === 0) {
+      return <div>There are no animals in the list!</div>;
+    }
+    
     return (
       <ul>
         {props.animals.map((animal) => {
