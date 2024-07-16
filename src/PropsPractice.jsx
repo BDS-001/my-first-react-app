a15d33e87372cbd59c4a06a964dc202256579570
BDS <113209967+BDS-001@users.noreply.github.com>
2024-07-16 16:06:09 -0400

    added props to button

--- src/PropsPractice.jsx
+++ src/PropsPractice.jsx
@@ -1,16 +1,21 @@
-function Button() {
+function Button(props) {
+    const buttonStyle = {
+        color: props.color,
+        fontSize: props.fontSize + 'px'
+    }
+
     return (
-        <button>Click Me!</button>
+        <button style={buttonStyle}>{props.text}</button>
     );
 }
 
 export function App() {
     return (
         <div>
-            <Button />
-            <Button />
-            <Button />
-            <Button />
+            <Button text="Click Me!" color="blue" fontSize={12} />
+            <Button text="Don't Click Me!" color="red" fontSize={12} />
+            <Button text="Click Me!" color="blue" fontSize={20} />
+            <Button text="Don't Click Me!" color="red" fontSize={20} />
         </div>
     )
 }
