3ece0a5a0fcb639c9f5c414f6811874833edfebf
BDS <113209967+BDS-001@users.noreply.github.com>
2024-07-16 15:16:23 -0400

    practice react keys

---
+++ src/TestingKeys.jsx
@@ -0,0 +1,15 @@
+const todos = [
+    { task: "mow the yard", id: 'unique-id-1' },
+    { task: "Work on Odin Projects", id: 'unique-id-2' },
+    { task: "feed the cat", id: 'unique-id-3'},
+  ];
+  
+  export function TodoList() {
+    return (
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.task}</li>
+        ))}
+      </ul>
+    );
+  }
