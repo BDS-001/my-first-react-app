e0c18b28f74ed39cc57efc08da85a38562e48bb4
BDS <113209967+BDS-001@users.noreply.github.com>
2024-07-16 16:14:40 -0400

    passed function as prop value

--- src/PropsPractice.jsx
+++ src/PropsPractice.jsx
@@ -1,19 +1,23 @@
-function Button({text = 'Click Me!', color = 'blue', fontSize = 12}) {
+function Button({text = 'Click Me!', color = 'blue', fontSize = 12, handleClick}) {
     const buttonStyle = {
         color: color,
         fontSize: fontSize + 'px'
     }
 
     return (
-        <button style={buttonStyle}>{text}</button>
+        <button onClick={handleClick} style={buttonStyle}>{text}</button>
     );
 }
 
 export function App() {
+    const handleButtonClick = () => {
+        window.location.href = 'https://www.theodinproject.com/lessons/node-path-react-new-passing-data-between-components'
+    }
+
     return (
         <div>
             <Button />
-            <Button text="Don't Click Me!" color="red" fontSize={12} />
+            <Button text="Don't Click Me!" color="red" fontSize={12} handleClick={handleButtonClick}/>
             <Button text="Click Me!" color="blue" fontSize={20} />
             <Button text="Don't Click Me!" color="red" fontSize={20} />
         </div>
