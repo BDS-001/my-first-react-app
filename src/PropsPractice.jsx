17e95ab14a9fbc5699e75eff3d7f540272e53677
BDS <113209967+BDS-001@users.noreply.github.com>
2024-07-16 15:50:48 -0400

    props setup

---
+++ src/PropsPractice.jsx
@@ -0,0 +1,16 @@
+function Button() {
+    return (
+        <button>Click Me!</button>
+    );
+}
+
+export function App() {
+    return (
+        <div>
+            <Button />
+            <Button />
+            <Button />
+            <Button />
+        </div>
+    )
+}
